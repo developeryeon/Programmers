06f028e7a1f33d2f8f468e32a080f6e8d8428454
보연 <141225736+developeryeon@users.noreply.github.com>
2024-01-04 11:28:55 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,15 @@
+function solution(num) {
+    var answer = '';
+    
+ 
+        if(num % 2 === 0){
+            return "Even";
+        }else {
+            return "Odd";
+        }
+ 
+    
+    return answer;
+}
+
+console.log(solution(4))
