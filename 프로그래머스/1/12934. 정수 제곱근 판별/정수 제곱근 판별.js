1aec4e085a7d59e0766607d46bebb351d3947f4a
보연 <141225736+developeryeon@users.noreply.github.com>
2024-01-17 09:32:22 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    // 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 한다.
+    // n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하라.
+    // Math.sqrt(n) : n의 제곱근을 구하는 메서드
+    let x = Math.sqrt(n);
+    
+    if(x % 1 === 0) {
+        return (x + 1)*(x + 1);
+    } else {
+        return -1;
+    }
+}
