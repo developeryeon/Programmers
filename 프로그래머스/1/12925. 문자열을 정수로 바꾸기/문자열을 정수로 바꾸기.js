290351aa0f621f4e069d4c329cb8dc45a7d20acc
보연 <141225736+developeryeon@users.noreply.github.com>
2024-01-17 09:08:07 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    // 문자열 s를 숫자로 변환한 결과를 반환하는 함수
+    // s의 길이 1 <= s <= 5
+    // str = "1234" 이면 1234를 반환, "-1234"이면 -1234를 반환하면 된다.
+    
+    let result = Number(s);
+    
+    return result;
+}
